db445dc3168cd95383b8b62620a766aa0d59bef5
Morraycage <sayf-r@hotmail.fr>
2017-07-12 22:14:59 +0200

    correct

--- messages/index.js
+++ messages/index.js
@@ -17,14 +17,34 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
 
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector [
+  function (session) {
+        session.send("Welcome to the dinner reservation.");
+        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "Who's name will this reservation be under?");
+    },
+    function (session, results) {
+        session.dialogData.reservationName = results.response;
+
+        // Process request and display reservation details
+        session.send("Reservation confirmed. Reservation details: <br/>Date/Time: %s <br/>Party size: %s <br/>Reservation name: %s",
+            session.dialogData.reservationDate, session.dialogData.partySize, session.dialogData.reservationName);
+        session.endDialog();
+    }
+]);
 
 bot.localePath(path.join(__dirname, './locale'));
 
 
 bot.dialog('/', [
     function (session) {
-        session.send("Bonjour");
         builder.Prompts.text(session, "Bonjour quel est le lieu?");
     },
     function (session, results) {
@@ -58,11 +78,9 @@ bot.dialog('/', [
                     " Type de Souvenir : " + session.userData.souvenir +
                     " Personne : " + session.userDate.personne +
                     " Tag: " + session.userDate.tag +
-                    " years and use " + session.userData.language +
-                    //"Picture:" + session.userData.picture + "."
-                  );
+                    " years and use " + session.userData.language + ".");
     }
-  ]);
+]);
 
 if (useEmulator) {
     var restify = require('restify');
